7b1d7c216a50a32d4af49244d8342abb49626275
Goriunovas <goriunovas@gmail.com>
2018-01-25 02:33:50 +0200

    Cors Enabled

--- Noted/Noted/App_Start/WebApiConfig.cs
+++ Noted/Noted/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Noted
 {
@@ -10,6 +11,9 @@ namespace Noted
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var corsAttr = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(corsAttr);
+
             config.Filters.Add(new AuthorizeAttribute());
             // Web API routes
             config.MapHttpAttributeRoutes();
--- Noted/Noted/Global.asax.cs
+++ Noted/Noted/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Routing;
 
 namespace Noted
@@ -11,6 +12,7 @@ namespace Noted
     {
         protected void Application_Start()
         {
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
